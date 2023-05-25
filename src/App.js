99723231a20fca301473546582f0d14ba08dfe2f
RajdeepDey010 <rdey5486@gmail.com>
2023-05-25 11:00:37 +0530

    react-app files added

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import React,{useState} from 'react';
+
+
+
+function App(){
+    // to store the todo data
+    let [todo, setTodo] = useState("");
+    let [list, setList] = useState([]);
+    
+
+    const addTodo = () => {
+        
+        setList([...list,todo]);
+        setTodo("");
+        console.log(todo);
+        console.log(list);
+        alert("Task: "+todo);
+    }
+
+    return(
+        <div className='App'>
+            <h2>TODO-APP</h2>
+
+            <div className='task-box'>
+                <input 
+                    className='task' 
+                    type='text' 
+                    value={todo}
+                    placeholder='Create a task' 
+                    onChange={(e) => {
+                        setTodo(e.target.value)
+                    }}
+                />
+                &nbsp;
+                <button className='add-task' onClick={addTodo}>Add</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
