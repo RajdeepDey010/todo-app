bf6489d07a21381ab4b690f6261fdb75e129a2b3
RajdeepDey010 <rdey5486@gmail.com>
2023-05-28 01:47:54 +0530

    added Add and Delete logic

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 
 
-function App(){
+function App() {
     // to store the todo data
     let [todo, setTodo] = useState("");
     let [list, setList] = useState([]);
-    
+
 
     const addTodo = () => {
-        
-        setList([...list,todo]);
+        setList([...list, todo]);
         setTodo("");
         console.log(todo);
         console.log(list);
-        alert("Task: "+todo);
     }
-    return(
+
+    const delTodo = (tsk) => {
+        /*  I found in my notebook where I recorded usage of different array methods, 
+            their filter() is a method which can return an array based on condition 
+            provided for original array.Let's use it here gain. */
+        const newList = list.filter((ele) => {
+            return ele !== tsk;
+        })
+        setList(newList);
+    }
+
+    return (
         <div className='App'>
             <h2>TODO-APP</h2>
-
-            <div className='task-box'>
-                <input 
-                    className='task' 
-                    type='text' 
+            <div className='add-todo'>
+                <input
+                    className='taskInp'
+                    type='text'
                     value={todo}
-                    placeholder='Create a task' 
+                    placeholder='Create a task'
                     onChange={(e) => {
                         setTodo(e.target.value)
                     }}
                 />
                 &nbsp;
-                <button className='add-task' onClick={addTodo}>Add</button>
+                <button className='add-btn' onClick={addTodo}>Add</button>
+
+            </div>
+            <div className='task-box'>
+                {list.length > 0 ? (
+                    <ul className="taskList">
+                        {
+                            list.map((item, index) => (
+                                <div className='task'>
+                                    <li key={index}>{item}</li>
+
+                                    &nbsp;
+                                    <button className='del-btn' onClick={() => delTodo(item)}>Delete</button>
+                                </div>
+                            ))
+                        }
+                    </ul>
+                ) : (
+                    <div>
+                        <h4>No task is added</h4>
+                    </div>
+                )
+                }
             </div>
         </div>
     );
